0b4df7fe21e57f961b52e53f1927db871d321ebc
CarolLiu <carolcoral@users.noreply.github.com>
2018-01-02 19:52:11 +0800

    Update DBUtils.java

--- ServerOnlineExamSystem/util/DBUtils.java
+++ ServerOnlineExamSystem/util/DBUtils.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ResourceBundle;
 
 public class DBUtils {
-	private static String DBCONFIGPATH = "cn.xdl.exam.util.db_config";// 配置文件的位置
+	// private static String DBCONFIGPATH = "cn.xdl.exam.util.db_config";//
+	// 配置文件的位置
 	private static String USERNAME;
 	private static String PWD;
 	private static String URL;
-	private static String DRIVER;
+	// private static String DRIVER;
 	// private static String MAXACTIVE;
 	// private static String MAXIDLE;
 	// private static String MAXWAIT;
@@ -20,20 +20,21 @@ public class DBUtils {
 
 	// 加载数据
 	static {
-		// 1.创建操作资源配置文件的对象
-		ResourceBundle rs = ResourceBundle.getBundle(DBCONFIGPATH);
-		// Properties properties = new Properties();
-		// properties.getProperty(DBCONFIGPATH);
-		// 2.从此对象中获取配置文件中的数据
-		USERNAME = rs.getString("username");
-		PWD = rs.getString("pwd");
-		URL = rs.getString("url");
-		DRIVER = rs.getString("driver");
-		// MAXACTIVE = properties.getProperty(key);
-		// 3. 注册数据库驱动
 		try {
-			Class.forName(DRIVER);
-		} catch (ClassNotFoundException e) {
+			// Class.forName(DRIVER);
+			// 1.创建操作资源配置文件的对象
+			// ResourceBundle rs = ResourceBundle.getBundle(DBCONFIGPATH);
+			// 2.从此对象中获取配置文件中的数据
+			// USERNAME = rs.getString("username");
+			// PWD = rs.getString("pwd");
+			// URL = rs.getString("url");
+			// DRIVER = rs.getString("driver");
+			USERNAME = "root";
+			PWD = "947752894";
+			URL = "jdbc:mysql://localhost:3306/OnlineExam";
+			// 3. 注册数据库驱动
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
